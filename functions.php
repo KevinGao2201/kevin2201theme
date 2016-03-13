1d3e49b57b52f22195bb5297e6102a49b999e75f
KevinGao2201 <kevingao.2201@gmail.com>
2016-03-13 13:24:57 -0700

    add customized typeface - commented out

--- functions.php
+++ functions.php
@@ -3,4 +3,11 @@ add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
+
+/*
+function load_my_fonts() {
+    wp_register_style('googleFonts', 'http://fonts.googleapis.com/css?family=Open+Sans'); 	wp_enqueue_style( 'googleFonts');
+}add_action('wp_print_styles', 'load_my_fonts');
+*/
+
 ?>
